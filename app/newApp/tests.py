bedaf63845349486b56e1a4cf50ac105c24bc9e5
Tejas Sutar <tejas.sutar@sskinnov.com>
2024-07-19 15:35:03 +0530

    db configuration

---
+++ app/newApp/tests.py
@@ -0,0 +1,7 @@
+from django.test import SimpleTestCase
+from newApp import calc
+
+class CalcTest(SimpleTestCase):
+    def test_calc(self):
+        res = calc.add(2, 3)
+        self.assertEqual(res, 5)
