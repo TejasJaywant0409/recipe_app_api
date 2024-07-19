ad1754de969a467f45dded8465d9898c191defe8
Tejas Sutar <tejas.sutar@sskinnov.com>
2024-07-19 19:39:01 +0530

    db configuration

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -1,10 +1,21 @@
 # django commands to wait for the db to be available.
 
-from typing import Any
+import time
+from psycopg2 import OperationalError as psycopg2OpError
+from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
 
-    def handle(self, *args: Any, **options: Any):
-        pass
+    def handle(self, *args, **options):
+        self.stdout.write('waiting for database...')
+        db_up = False
+        while db_up is False:
+            try:
+                self.check(databases=['default'])
+                db_up = True
+            except (psycopg2OpError, OperationalError):
+                self.stdout.write('database unavailable, wait for 1 second...')
+                time.sleep(1)
+        self.stdout.write(self.style.SUCCESS('database available!'))
--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -1,12 +1,24 @@
 from django.test import SimpleTestCase
 from unittest.mock import patch
-# from psycopg2 import OperationalError as psycopg2Error
-# from django.core.management import call_command
-# from django.db.utils import OperationalError
+from django.core.management import call_command
+from psycopg2 import OperationalError as psycopg2OpError
+from django.db.utils import OperationalError
 
 
-@patch()
+@patch('core.management.commands.wait_for_db.Command.check')
 class CommandsTests(SimpleTestCase):
 
-    def test_wait_for_db(self):
-        pass
+    def test_wait_for_db_ready(self, patched_check):
+        patched_check.return_value = True
+        call_command('wait_for_db')
+
+        patched_check.assert_called_once_with(databases=['default'])
+
+    @patch('time.sleep')
+    def test_wait_for_db_delay(self, patched_sleep, patched_check):
+        patched_check.side_effect = [psycopg2OpError] * 2 + \
+            [OperationalError] * 3 + [True]
+        call_command('wait_for_db')
+
+        # Check if the patched_check was called 6 times
+        self.assertEqual(patched_check.call_count, 6)
