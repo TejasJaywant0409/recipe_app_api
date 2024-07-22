555548b8cbd1a9ade6fedd54a7c42481586ce709
Tejas Sutar <tejas.sutar@sskinnov.com>
2024-07-22 18:41:12 +0530

    test model

--- app/core/models.py
+++ app/core/models.py
@@ -22,7 +22,7 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
 
         return user
-    
+
     def create_superuser(self, email, password):
         user = self.create_user(email, password)
         user.is_superuser = True
@@ -34,7 +34,7 @@ class UserManager(BaseUserManager):
 
 class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(max_length=255, unique=True)
-    password = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
